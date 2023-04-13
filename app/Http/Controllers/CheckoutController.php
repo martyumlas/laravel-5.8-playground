d77b311a88bddb4d2ff54c11d0b36a8e5e60f7c4
LAPTOP-M9KEM7H7\Marty <martyumlas@gmail.com>
2023-04-13 15:52:41 +0800

    update endpoint using digital pay

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
@@ -14,22 +16,29 @@ class CheckoutController extends Controller
         $price = $request->price;
         $invoice = $request->invoice;
         $option = $request->option;
-
+        $espaysignature = 'sc8n9j0vmqa0ejzi';
+        $rq_datetime = Carbon::now()->format('Y-m-d H:i:s');
+        $ccy = 'IDR';
+        $comm_code = 'SGWPDSINTERNATIONAL';
+        $mode = 'SENDINVOICE';
+        $salt = strtoupper("##$espaysignature##$invoice##$rq_datetime##$price##$ccy##$comm_code##$mode##");
+        $signature = hash('sha256', $salt);
+     
+     
         if($option == 1) {
             
             return view('checkout', compact('price', 'invoice', 'option'));
         } else {
-            $client = new Client([
-                'base_uri' => 'https://sandbox-kit.espay.id/index/order/',
-            ]);
-
-            $response = $client->request('GET', 'endpoint', [
-                'query' => [
-                    'productCode' => 002,
-                    'commCode' => 'SGWPDSINTERNATIONAL'
-                ]
-            ]);
-
+            $client = new Client();
+            try {
+                $response = $client->post('https://sandbox-api.espay.id/rest/digitalpay/pushtopay', [
+                   
+                ]);
+    
+            } catch(Exception $e) {
+                return $e->getMessage();
+            }
+            
             $data = $response->getBody()->getContents();
 
             return $data;
